0fc1ebfc5403def23153c168426b535eeb583827
Divyansh Jindal <divyanshjindal18@gmail.com>
2024-01-20 19:49:00 +0530

    Medium more

---
+++ Arrays/Medium/Rotate_Image.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/rotate-image/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
---
+++ Arrays/Medium/Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraySum(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int sum = 0;
+    int ct = 0;
+    unordered_map<int, int> mpp;
+    mpp[0] = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += nums[i];
+        int rem = sum - k;
+        ct += mpp[rem];
+        mpp[sum]++;
+    }
+    return ct;
+}
