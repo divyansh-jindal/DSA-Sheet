cb8218c550fd22cff9b5622c05460458ad0de3cf
Divyansh Jindal <75629015+divyansh-jindal@users.noreply.github.com>
2024-01-22 16:51:07 +0530

    Time: 15 ms (79.76%), Space: 21.5 MB (6.09%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        if(arr.size()==1) return;
+        int j=-1;
+        for(int i=0;i<arr.size();i++) {
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        };
+        if(j==-1) return;
+        for(int i=j+1;i<arr.size();i++){
+            if(arr[i]!=0){
+                swap(arr[j],arr[i]);
+                j++;
+            }
+        }
+    }
+};
