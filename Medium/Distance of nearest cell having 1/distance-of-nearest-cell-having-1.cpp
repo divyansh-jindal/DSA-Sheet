3e0ef325de02d879beaa75b691b90148ebfec6f9
Divyansh Jindal <75629015+divyansh-jindal@users.noreply.github.com>
2024-03-10 09:36:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    // Code here
+	    int n = grid.size();
+	    int m = grid[0].size();
+	    
+	    vector<vector<int>> vis(n,vector<int>(m,0));
+	    vector<vector<int>> dis(n,vector<int>(m,0));
+	    
+	    queue<pair<pair<int,int>,int>> q;
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(grid[i][j]){
+	                q.push({{i,j},0});
+	                vis[i][j]=1;
+	            }
+	        }
+	    }
+	    
+	    int dr[]={1,0,-1,0};
+	    int dc[]={0,1,0,-1};
+	    
+	    while(!q.empty()){
+	        int row = q.front().first.first;
+	        int col = q.front().first.second;
+	        int dist = q.front().second;
+	        q.pop();
+	        
+	       // vis[row][col]=1;
+	        dis[row][col]=dist;
+	        
+	        for(int i=0;i<4;i++){
+	            int nr=row+dr[i];
+	            int nc=col+dc[i];
+	            
+	            if(nr>=0 and nr<n and nc>=0 and nc<m and !vis[nr][nc] and !grid[nr][nc]){
+	                q.push({{nr,nc},dist+1});
+	                vis[nr][nc]=1;
+	            }
+	        }
+	    }
+	    
+	    return dis;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.nearest(grid);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
